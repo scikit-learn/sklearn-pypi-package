b8a8f407c748d5000c0442360744f4b1e17cc85c
Loïc Estève <loic.esteve@ymail.com>
2022-10-10 16:47:26 +0200

    Initial commit with sklearn 0.0 content

---
+++ README.md
@@ -0,0 +1,8 @@
+**Note:** `sklearn` package on PyPI exists to prevent bad actors to use the
+name. `scikit-learn` is the actual package name and should be used with pip,
+e.g. for:
+- pip commands: `pip install scikit-learn`
+- pip `requirements.txt` files
+
+The aim of this repo is to implement the brownout of the `sklearn` package on
+PyPI.
---
+++ setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='UNKNOWN',
+    author_email="UNKNOWN\nblas_opt_info:\nblas_mkl_info:\n  FOUND:\n    libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core', 'iomp5', 'pthread']\n    library_dirs = ['/Users/filo/anaconda/lib']\n    define_macros = [('SCIPY_MKL_H', None)]\n    include_dirs = ['/Users/filo/anaconda/include']\n\n  FOUND:\n    libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core', 'iomp5', 'pthread']\n    library_dirs = ['/Users/filo/anaconda/lib']\n    define_macros = [('SCIPY_MKL_H', None)]\n    include_dirs = ['/Users/filo/anaconda/include']\n\nUNKNOWN",
+    description='A set of python modules for machine learning and data mining',
+    install_requires=['scikit-learn'],
+    long_description='''Use `scikit-learn <https://pypi.python.org/pypi/scikit-learn/>`_ instead.''',
+    maintainer='UNKNOWN',
+    maintainer_email="UNKNOWN\nblas_opt_info:\nblas_mkl_info:\n  FOUND:\n    libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core', 'iomp5', 'pthread']\n    library_dirs = ['/Users/filo/anaconda/lib']\n    define_macros = [('SCIPY_MKL_H', None)]\n    include_dirs = ['/Users/filo/anaconda/include']\n\n  FOUND:\n    libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core', 'iomp5', 'pthread']\n    library_dirs = ['/Users/filo/anaconda/lib']\n    define_macros = [('SCIPY_MKL_H', None)]\n    include_dirs = ['/Users/filo/anaconda/include']\n\nUNKNOWN",
+    name='sklearn',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/scikit-learn/',
+    version="0.0",
+    zip_safe=False,
+)
