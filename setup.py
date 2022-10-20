e0df7fe664a4e802270c7091d681bf2ac1bff173
Loïc Estève <loic.esteve@ymail.com>
2022-10-20 12:04:14 +0200

    Simpler logic to always error except for sdist
    Also added an environment variable for test purposes

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import datetime, MAXYEAR
 from collections import namedtuple
 
@@ -85,18 +86,25 @@ def maybe_raise_error(checked_datetime):
             raise SystemExit(error_message)
 
 
-class AbortInstall(install):
-    def run(self):
-        now = datetime.now()
-        maybe_raise_error(now)
-
-
 if __name__ == "__main__":
+    # We allow python setup.py sdist to always work to be able to create the
+    # sdist and upload it to PyPI
+    sdist_mode = len(sys.argv) == 2 and sys.argv[1] == 'sdist'
+
+    if not sdist_mode:
+        # environment variable for test purposes
+        datetime_str = os.getenv('SKLEARN_DEPRECATED_SKLEARN_CHECKED_DATETIME', None)
+        if datetime_str is None:
+            checked_datetime = datetime.now()
+        else:
+            checked_datetime = datetime.fromisoformat(datetime_str)
+
+        maybe_raise_error(checked_datetime)
+
     setup(
-        cmdclass={"install": AbortInstall},
         description="deprecated sklearn package, use scikit-learn instead",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         name="sklearn",
-        version="0.1.0a2",
+        version="0.1.0a6",
     )
