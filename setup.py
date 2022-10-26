f609c706d57991fcd5283bf1e59b482e1971ef7d
Loïc Estève <loic.esteve@ymail.com>
2022-10-26 18:01:50 +0200

    Use 0.0.post1 as version
    Close #2

--- setup.py
+++ setup.py
@@ -106,5 +106,5 @@ if __name__ == "__main__":
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         name="sklearn",
-        version="0.1.0a6",
+        version="0.0.post1",
     )
