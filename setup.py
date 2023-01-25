6db95071b875fbe32d94e432262ef928c870b353
Loïc Estève <loic.esteve@ymail.com>
2023-01-25 08:50:44 +0100

    Avoid pip wheel caching (#4)

--- setup.py
+++ setup.py
@@ -2,10 +2,27 @@ import os
 import sys
 from datetime import datetime, MAXYEAR
 from collections import namedtuple
+import setuptools
 
-from setuptools import setup
-from setuptools.command.install import install
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
+    class bdist_wheel(_bdist_wheel):
+        def run(self):
+            message = "\n".join(
+                [
+                    "This is an expected error. Building wheel is disabled "
+                    "for the deprecated sklearn PyPI package to avoid pip caching.",
+                    "For more details about the sklearn PyPI package deprecation, see:",
+                    "https://github.com/scikit-learn/sklearn-pypi-package",
+                ]
+            )
+            raise setuptools.errors.ClassError(message)
+
+    cmdclass = {"bdist_wheel": bdist_wheel}
+
+except ImportError:
+    cmdclass = {}
 
 with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
@@ -101,10 +118,11 @@ if __name__ == "__main__":
 
         maybe_raise_error(checked_datetime)
 
-    setup(
+    setuptools.setup(
         description="deprecated sklearn package, use scikit-learn instead",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         name="sklearn",
         version="0.0.post1",
+        cmdclass=cmdclass,
     )
