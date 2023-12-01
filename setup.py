878547c03764368c1ca6ce92259f398b08c1fb30
Loïc Estève <loic.esteve@ymail.com>
2023-12-01 15:10:05 +0100

    Update after end of brownout period

--- setup.py
+++ setup.py
@@ -1,45 +1,17 @@
 import os
 import sys
-from datetime import datetime, MAXYEAR
-from collections import namedtuple
+import warnings
 
 from setuptools import setup
 
 
-with open("README.md", encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
 
-def get_brownout_schedule():
-    all_start_dates = [datetime(2022, 12, 1)] + [
-        datetime(2023, 2 * i, 1) for i in range(1, 7)
-    ]
-    date_in_the_far_future = datetime(MAXYEAR, 12, 31, 23, 59, 59)
-    all_end_dates = all_start_dates[1:] + [date_in_the_far_future]
-    all_check_functions = [
-        lambda dt: dt.minute < 5,
-        lambda dt: dt.minute < 10,
-        lambda dt: dt.minute < 15,
-        lambda dt: (dt.minute < 10) or (30 <= dt.minute < 40),
-        lambda dt: (dt.minute < 15) or (30 <= dt.minute < 45),
-        lambda dt: (dt.minute < 20) or (30 <= dt.minute < 50),
-        lambda dt: True,
-    ]
-
-    CheckedDatetimeWindow = namedtuple(
-        "CheckedDatetimeWindow", "start_datetime end_datetime check_function"
-    )
-    brownout_schedule = [
-        CheckedDatetimeWindow(*each)
-        for each in zip(all_start_dates, all_end_dates, all_check_functions)
-    ]
-
-    return brownout_schedule
-
-
-def maybe_raise_error(checked_datetime):
+def likely_error():
     accept_deprecated_sklearn_package_install = os.environ.get(
-        "SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL", "use-brownout-schedule"
+        "SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL", "unset"
     )
     accept_deprecated_sklearn_package_install = (
         accept_deprecated_sklearn_package_install.lower()
@@ -47,12 +19,6 @@ def maybe_raise_error(checked_datetime):
 
     if accept_deprecated_sklearn_package_install == "true":
         return
-
-    if accept_deprecated_sklearn_package_install == "false":
-        raise SystemExit(
-            "Refusing to install the deprecated sklearn package, "
-            "because SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=False is set"
-        )
 
     error_message = "\n".join(
         [
@@ -71,18 +37,10 @@ def maybe_raise_error(checked_datetime):
             "",
             "More information is available at",
             "https://github.com/scikit-learn/sklearn-pypi-package",
-            "",
-            "If the previous advice does not cover your use case, feel free to report it at",
-            "https://github.com/scikit-learn/sklearn-pypi-package/issues/new",
         ]
     )
 
-    brownout_schedule = get_brownout_schedule()
-    for start_date, end_date, check_function in brownout_schedule:
-        if (start_date <= checked_datetime < end_date) and check_function(
-            checked_datetime
-        ):
-            raise SystemExit(error_message)
+    raise SystemExit(error_message)
 
 
 if __name__ == "__main__":
@@ -91,19 +49,12 @@ if __name__ == "__main__":
     sdist_mode = len(sys.argv) == 2 and sys.argv[1] == "sdist"
 
     if not sdist_mode:
-        # environment variable for test purposes
-        datetime_str = os.getenv("SKLEARN_DEPRECATED_SKLEARN_CHECKED_DATETIME", None)
-        if datetime_str is None:
-            checked_datetime = datetime.now()
-        else:
-            checked_datetime = datetime.fromisoformat(datetime_str)
-
-        maybe_raise_error(checked_datetime)
+        likely_error()
 
     setup(
         description="deprecated sklearn package, use scikit-learn instead",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
         name="sklearn",
-        version="0.0.post11",
+        version="0.0.post12",
     )
